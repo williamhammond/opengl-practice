fb19f49f0c44f2158aa9d65df067e7af366346e1
William Hammond <william.t.hammond@gmail.com>
2022-09-28 15:56:22 -0400

    Adds basic window loop

--- main.cpp
+++ main.cpp
@@ -1,9 +1,55 @@
 #include <iostream>
 
-//#include <GLFW/glfw3.h>
 #include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+// Window dimensions
+const GLint WIDTH = 800;
+const GLint HEIGHT = 600;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    if (!glfwInit()) {
+        printf("GLFW initialization failed!");
+        glfwTerminate();
+        return 1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    auto *mainWindow = glfwCreateWindow(WIDTH, HEIGHT, "Test Window", nullptr, nullptr);
+    if (!mainWindow) {
+        printf("GLFW window creation failed!");
+        glfwTerminate();
+        return 1;
+    }
+
+    int bufferWidth;
+    int bufferHeight;
+    glfwGetFramebufferSize(mainWindow, &bufferWidth, &bufferHeight);
+    glfwMakeContextCurrent(mainWindow);
+
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK) {
+        printf("Glew initialization failed");
+        glfwDestroyWindow(mainWindow);
+        glfwTerminate();
+        return 1;
+    }
+
+    glViewport(0, 0, bufferWidth, bufferHeight);
+
+    while (!glfwWindowShouldClose(mainWindow)) {
+        glfwPollEvents();
+
+        glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(mainWindow);
+    }
+
     return 0;
 }
